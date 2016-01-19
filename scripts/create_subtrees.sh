86ca4e2abd282ee0a72e939359184b027977d526
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-01-19 23:45:30 +0100

    Add subtree script

---
+++ scripts/create_subtrees.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -f
+../create_subtree.sh EXT_systemc
+../create_subtree.sh EXT_xmlParser
+../create_subtree.sh lib_bmp
+../create_subtree.sh parameter_manager
+../create_subtree.sh quicky_exception
+../create_subtree.sh quicky_tools
+../create_subtree.sh quicky_utils
+../create_subtree.sh simple_gui
+../create_subtree.sh wireworld
+../create_subtree.sh wireworld_common
+../create_subtree.sh wireworld_computer
+../create_subtree.sh wireworld_systemc
+../create_subtree.sh wireworld_tool
+#EOF
